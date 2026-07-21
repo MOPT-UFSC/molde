453110015cdb5c05a9d9f3e0f7af0075179f1771
André Fernandes <fpf.andre@gmail.com>
2026-07-21 10:34:58 -0300

    Add .ico files into the conversion script

--- scripts/convert.py
+++ scripts/convert.py
@@ -2,9 +2,11 @@ import zipfile
 from pathlib import Path
 
 import fitz  # PyMuPDF
+from PIL import Image
 
 
 def svg_to_png(input_path: Path, output_path: Path, dpi=300):
+    output_path.parent.mkdir(parents=True, exist_ok=True)
     doc = fitz.open(input_path)
     page = doc.load_page(0)
     pix = page.get_pixmap(dpi=dpi, alpha=True)
@@ -12,8 +14,25 @@ def svg_to_png(input_path: Path, output_path: Path, dpi=300):
     doc.close()
 
 
+def png_to_ico(input_path: Path, output_path: Path):
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    img = Image.open(input_path)
+    img.save(
+        output_path,
+        format="ICO",
+        sizes=[
+            (16, 16),
+            (32, 32),
+            (48, 48),
+            (64, 64),
+            (128, 128),
+            (256, 256),
+        ],
+    )
+
+
 def zip_folders(*folders: tuple[Path], output: Path):
-    with zipfile.ZipFile(output, "w", zipfile.ZIP_DEFLATED) as zipf:
+    with zipfile.ZipFile(output, "w", zipfile.ZIP_DEFLATED) as zip:
         for folder in folders:
             folder = Path(folder)
 
@@ -26,13 +45,10 @@ def zip_folders(*folders: tuple[Path], output: Path):
                     continue
 
                 relative_path = file.relative_to(base_path)
-                zipf.write(file, relative_path)
+                zip.write(file, relative_path)
 
 
 def main():
-    svg_base_path = Path("data/svg/")
-    png_base_path = Path("data/png/")
-
     for svg_path in Path("data/open_pulse/svg").glob("*.svg"):
         png_path = Path("data/open_pulse/png") / svg_path.with_suffix(".png").name
         svg_to_png(svg_path, png_path)
@@ -40,6 +56,14 @@ def main():
     for svg_path in Path("data/vibra/svg").glob("*.svg"):
         png_path = Path("data/vibra/png") / svg_path.with_suffix(".png").name
         svg_to_png(svg_path, png_path)
+
+    for png_path in Path("data/open_pulse/png").glob("open_pulse_circle_*.png"):
+        ico_path = Path("data/open_pulse/ico") / png_path.with_suffix(".ico").name
+        png_to_ico(png_path, ico_path)
+
+    for png_path in Path("data/vibra/png").glob("vibra_circle_*.png"):
+        ico_path = Path("data/vibra/ico") / png_path.with_suffix(".ico").name
+        png_to_ico(png_path, ico_path)
 
     zip_folders("data/vibra", "data/open_pulse", output="data/all_logos.zip")
 
